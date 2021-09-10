e3c3655738c81da18f9422548245a6d72cebd28c
virtuelles <88047603+virtuelles@users.noreply.github.com>
2021-09-10 15:52:49 +0800

    params簡化,例外處理一般影片,輸出修改
    一般影片沒有liveStreamingDetails,例外處理
    改用藍綠色輸出
    一些變數名稱修改
    X軸修改
    一些空格

--- countAfterStream.py
+++ countAfterStream.py
@@ -26,14 +26,14 @@ def sleep_time(hour, min, sec):
 
 def get_livestream_info(API_KEY,vID):
     '''
-     Use "videos" function to derive info of livestream
+    Use "videos" function to derive info of livestream
     '''
     params = {
-              'part': 'liveStreamingDetails,statistics,snippet',
-              'key': API_KEY,
-              'id': vID,
-              'fields': 'items(id,snippet(title,channelTitle,publishedAt,liveBroadcastContent),statistics,liveStreamingDetails)'
-              }
+            'part': 'liveStreamingDetails,statistics,snippet',
+            'key': API_KEY,
+            'id': vID,
+            'fields': 'items(id,snippet(publishedAt,channelId,title,channelTitle,,liveBroadcastContent),statistics,liveStreamingDetails)'
+            }
     headers = {'User-Agent': 'Chrome/92.0.4515.107'}
     url = 'https://www.googleapis.com/youtube/v3/videos'
     req = requests.get(url, headers=headers, params=params).json()
@@ -69,26 +69,35 @@ else:
     os.mkdir(channelFileName)
     os.chdir(channelFileName)
 
-if "scheduledStartTime" in sinfo['liveStreamingDetails']:
-    Planed_Start_Time=sinfo['liveStreamingDetails']['scheduledStartTime']
-    Planed_Start_Time=APItimeSetToUTC8(Planed_Start_Time)
-else:
-    Planed_Start_Time='NaN'
+try:
+    if "scheduledStartTime" in sinfo['liveStreamingDetails']:
+        Planed_Start_Time=sinfo['liveStreamingDetails']['scheduledStartTime']
+        Planed_Start_Time=APItimeSetToUTC8(Planed_Start_Time)
+    else:
+        Planed_Start_Time='NaN'
 
-if "actualStartTime" in sinfo['liveStreamingDetails']:
-    Actual_Start_Time=sinfo['liveStreamingDetails']['actualStartTime']
-    Actual_Start_Time=APItimeSetToUTC8(Actual_Start_Time)
+    if "actualStartTime" in sinfo['liveStreamingDetails']:
+        Actual_Start_Time=sinfo['liveStreamingDetails']['actualStartTime']
+        Actual_Start_Time=APItimeSetToUTC8(Actual_Start_Time)
+except:
+    Planed_Start_Time='NaN'
+    Actual_Start_Time='NaN'
 
 #檢查直播是否開始
 liveBroadcastContent=sinfo['snippet']['liveBroadcastContent']
 
 Title=sinfo['snippet']['title']
 TitleFileName=re.sub('[\/:*?"<>|]',' ',Title)
-Actual_Start_Time=sinfo['liveStreamingDetails']['actualStartTime']
-Actual_Start_Time=APItimeSetToUTC8(Actual_Start_Time)
+try:
+    Actual_Start_Time=sinfo['liveStreamingDetails']['actualStartTime']
+    Actual_Start_Time=APItimeSetToUTC8(Actual_Start_Time)
 
-ActualStartTimeFileName=datetime.datetime.strftime(Actual_Start_Time,'%Y-%m-%d %H:%M:%S ')
-ActualStartTimeFileName=ActualStartTimeFileName[0:10]
+    ActualStartTimeFileName=datetime.datetime.strftime(Actual_Start_Time,'%Y-%m-%d %H:%M:%S ')
+    ActualStartTimeFileName=ActualStartTimeFileName[0:10]
+except:
+    Actual_Start_Time='NaN'
+    ActualStartTimeFileName=datetime.datetime.strftime(Stream_Created,'%Y-%m-%d %H:%M:%S ')
+    ActualStartTimeFileName=ActualStartTimeFileName[0:10]
 
 if os.path.isdir(ActualStartTimeFileName+' '+TitleFileName):
     os.chdir(ActualStartTimeFileName+' '+TitleFileName)
@@ -96,13 +105,13 @@ else:
     os.mkdir(ActualStartTimeFileName+' '+TitleFileName)
     os.chdir(ActualStartTimeFileName+' '+TitleFileName)
 
-#開啟csv檔寫入直播數據
+#開啟csv檔寫入直播後數據
 countAfterStream=open('count after stream of '+TitleFileName+'.csv', 'a', newline='', encoding='UTF-8')
 #countAfterStream.write('time,viewCount,likeCount,dislikeCount\n')
 
-#開啟txt檔寫入直播資訊
+#開啟txt檔寫入直播後資訊
 countInfo=open('count after stream info of '+TitleFileName+'.txt', 'a', newline='', encoding='UTF-8')
-streamInfoCollectstart=['頻道 Channel:',Channel,'\n','標題 Title:',Title,'\n','影片ID Video_ID:',Video_ID,'\n','直播公開時間 Stream Created time(UTC+8):',str(Stream_Created),'\n','預定開始時間 Planed Start Time(UTC+8):',str(Planed_Start_Time),'\n']
+streamInfoCollectstart=['count after stream: 1 day version\n','頻道 Channel:',Channel,'\n','標題 Title:',Title,'\n','影片ID Video_ID:',Video_ID,'\n','直播公開時間 Stream Created time(UTC+8):',str(Stream_Created),'\n','預定開始時間 Planed Start Time(UTC+8):',str(Planed_Start_Time),'\n']
 countInfo.writelines(streamInfoCollectstart)
 
 streamInfoCollectAST=['實際開始時間 Actual Start Time(UTC+8):',str(Actual_Start_Time),'\n']
@@ -111,8 +120,9 @@ Log_Start_Time = datetime.datetime.now().replace(microsecond=0).isoformat()
 Log_Start_Time=localTimeClean(Log_Start_Time)
 countInfo.write('紀錄開始時間 Log Start Time(UTC+8):'+Log_Start_Time+'\n')
 
-#寫入csv,todo固定紀錄時間
-while liveBroadcastContent=='none':
+#寫入csv,預設一天,144*10min
+
+for i in range(144):
 
     try:
         localtime = datetime.datetime.now().replace(microsecond=0).isoformat()
@@ -145,6 +155,7 @@ while liveBroadcastContent=='none':
         
         second = sleep_time(0, 10, 0)
         time.sleep(second)
+
     except KeyboardInterrupt:
         logStopTime = datetime.datetime.now().replace(microsecond=0).isoformat()
         logStopTime=localTimeClean(logStopTime)
@@ -158,11 +169,15 @@ countAfterStream.close()
 logStopTime = datetime.datetime.now().replace(microsecond=0).isoformat()
 logStopTime=localTimeClean(logStopTime)
 #info寫入
-if "actualEndTime" in sinfo['liveStreamingDetails']:
-    Actual_End_Time=sinfo['liveStreamingDetails']['actualEndTime']
-    Actual_End_Time=APItimeSetToUTC8(Actual_End_Time)
-    countInfo.write('直播結束時間 Actual End Time(UTC+8):'+str(Actual_End_Time)+'\n')
-else:
+try:
+    if "actualEndTime" in sinfo['liveStreamingDetails']:
+        Actual_End_Time=sinfo['liveStreamingDetails']['actualEndTime']
+        Actual_End_Time=APItimeSetToUTC8(Actual_End_Time)
+        countInfo.write('直播結束時間 Actual End Time(UTC+8):'+str(Actual_End_Time)+'\n')
+    else:
+        Actual_End_Time='NaN'
+        countInfo.write('直播結束時間 Actual End Time(UTC+8):NaN\n')
+except:
     Actual_End_Time='NaN'
     countInfo.write('直播結束時間 Actual End Time(UTC+8):NaN\n')
 
@@ -196,67 +211,67 @@ chartTitle='Channel: '+Channel+'\n'+'Title: '+Title+'\n'+'Count after stream'
 
 #這裡應該能簡化
 headers=['time','viewCount','likeCount','dislikeCount','commentCount']
-df = pd.read_csv('count after stream of '+TitleFileName+'.csv', names=headers,header=None,usecols=[0,1,2,3,4])
+dfafter = pd.read_csv('count after stream of '+TitleFileName+'.csv', names=headers,header=None,usecols=[0,1,2,3,4])
 
-t = df['time']
+t = dfafter['time']
 
-v = df['viewCount']
-l = df['likeCount']
-d = df['dislikeCount']
-c = df['commentCount']
+v = dfafter['viewCount']
+l = dfafter['likeCount']
+d = dfafter['dislikeCount']
+c = dfafter['commentCount']
 
-MaxViewCount=df['viewCount'].max()
-MaxLikeCount=df['likeCount'].max()
-MaxDislikeCount=df['dislikeCount'].max()
-MaxCommentCount=df['commentCount'].max()
+MaxViewCount=dfafter['viewCount'].max()
+MaxLikeCount=dfafter['likeCount'].max()
+MaxDislikeCount=dfafter['dislikeCount'].max()
+MaxCommentCount=dfafter['commentCount'].max()
 countResult='MaxLikeCount:{0}\nMaxDislikeCount:{1}\nMaxViewCount:{2}\nMaxCommentCount:{3}'.format(MaxLikeCount,MaxDislikeCount,MaxViewCount,MaxCommentCount)
 timeResult='Actual Start Time(UTC+8):{0}\nActual End Time(UTC+8):{1}\nLog Start Time(UTC+8):{2}\nLog Stop Time(UTC+8):{3}'.format(Actual_Start_Time,Actual_End_Time,Log_Start_Time,logStopTime)
 
 #標題副標題,圖表結構,部分頻道和標題因字型問題無法完整顯示
-fig, (ax1, ax2, ax3) = plt.subplots(3,sharex=True,figsize=(12.8,10.8))
-fig.suptitle(chartTitle,fontname="MS Gothic", fontsize=15)
-fig.set_facecolor('wheat')
-ax1.set_facecolor('moccasin')
-ax1.set_title(countResult,loc='left', fontsize=12)
-ax1.set_title(timeResult,loc='right', fontsize=12)
+fig2, (ax3, ax4, ax5) = plt.subplots(3,sharex=True,figsize=(12.8,10.8))
+fig2.suptitle(chartTitle,fontname="MS Gothic", fontsize=15)
+fig2.set_facecolor('paleturquoise')
+ax3.set_facecolor('lightcyan')
+ax3.set_title(countResult,loc='left', fontsize=12)
+ax3.set_title(timeResult,loc='right', fontsize=12)
 
 #圖表1
-ax1.set_ylabel('count')
+ax3.set_ylabel('count')
 
-ax1.plot(t,l,'g',label='likeCount')
-ax1.plot(t,d,'r',label='dislikeCount')
-ax1.legend(loc='upper left')
-ax1.ticklabel_format(axis='y', style='plain')
-ax1.grid(True)
-
-#圖表2
-ax2.set_facecolor('moccasin')
-ax2.plot(t,v,'c',label='viewCount')
-ax2.legend(loc='upper left')
-ax2.ticklabel_format(axis='y', style='plain')
-ax2.set_ylabel('count')
-ax2.grid(True)
-
-#圖表3
-ax3.set_facecolor('moccasin')
-ax3.plot(t,c,'darkcyan',label='commentCount')
+ax3.plot(t,l,'g',label='likeCount')
+ax3.plot(t,d,'r',label='dislikeCount')
 ax3.legend(loc='upper left')
 ax3.ticklabel_format(axis='y', style='plain')
-ax3.set_ylabel('count')
 ax3.grid(True)
 
+#圖表2
+ax4.set_facecolor('lightcyan')
+ax4.plot(t,v,'c',label='viewCount')
+ax4.legend(loc='upper left')
+ax4.ticklabel_format(axis='y', style='plain')
+ax4.set_ylabel('count')
+ax4.grid(True)
+
+#圖表3
+ax5.set_facecolor('lightcyan')
+ax5.plot(t,c,'darkcyan',label='commentCount')
+ax5.legend(loc='upper left')
+ax5.ticklabel_format(axis='y', style='plain')
+ax5.set_ylabel('count')
+ax5.grid(True)
+
 #X軸簡化
-x = df.time
+x = dfafter.time
 ticker_spacing = x
-ticker_spacing = 160
-ax2.xaxis.set_major_locator(ticker.MultipleLocator(ticker_spacing))
+ticker_spacing = 6
+ax5.xaxis.set_major_locator(ticker.MultipleLocator(ticker_spacing))
 plt.xticks(rotation=20, fontsize=7)
 plt.xlabel('time')
 
 #儲存圖表
-fig.tight_layout()
+fig2.tight_layout()
 print('---儲存折線圖---')
-plt.savefig('count chart after stream of' + TitleFileName + '.png')
+plt.savefig('count chart after stream of ' + TitleFileName + '.png')
 print('---儲存完成---')
 
 #結束後輸出
@@ -267,24 +282,27 @@ if "publishedAt" in sinfo['snippet']:
     print('直播公開時間 Stream Created Time(UTC+8):', Stream_Created)
 else:
     print('直播公開時間 Stream Created Time(UTC+8):NaN')
+try:
+    if "scheduledStartTime" in sinfo['liveStreamingDetails']:
+        print('預定開始時間 Planed Start Time(UTC+8):', Planed_Start_Time)
+    else:
+        print('預定開始時間 Planed Start Time(UTC+8):NaN')
 
-if "scheduledStartTime" in sinfo['liveStreamingDetails']:
-    print('預定開始時間 Planed Start Time(UTC+8):', Planed_Start_Time)
-else:
+    if "actualStartTime" in sinfo['liveStreamingDetails']:
+        print('實際開始時間 Actual Start Time(UTC+8):', Actual_Start_Time)
+    else:
+        print('實際開始時間 Actual Start Time(UTC+8):NaN')
+
+    if "actualEndTime" in sinfo['liveStreamingDetails']:
+        print('直播結束時間 Actual End Time(UTC+8):', Actual_End_Time)
+    else:
+        print('直播結束時間 Actual End Time(UTC+8):NaN')
+except:
     print('預定開始時間 Planed Start Time(UTC+8):NaN')
-
-if "actualStartTime" in sinfo['liveStreamingDetails']:
-    print('實際開始時間 Actual Start Time(UTC+8):', Actual_Start_Time)
-else:
     print('實際開始時間 Actual Start Time(UTC+8):NaN')
+    print('直播結束時間 Actual End Time(UTC+8):NaN')
 
-if "actualEndTime" in sinfo['liveStreamingDetails']:
-    print('直播結束時間 Actual End Time(UTC+8):', Actual_End_Time)
-else:
-    print('直播結束時間 Actual End Time(UTC+8):NaN\n')
-
-
-print('紀錄結束時間 Log Stop Time(UTC+8): '+str(logStopTime))
+print('紀錄結束時間 Log Stop Time(UTC+8): '+str(logStopTime)+'\n')
 
 if "viewCount" in sinfo['statistics']:
     print('紀錄結束時播放數 View Count:', sinfo['statistics']['viewCount'])
